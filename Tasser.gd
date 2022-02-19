6692b0fef0f2a37262c184908acfbdf56043b603
Torben Lochow <t.lochow@gmx.de>
2022-02-19 22:45:39 +0100

    Removed Tasser scene.

--- Tasser.gd
+++
@@ -1,9 +0,0 @@
-extends AnimationPlayer
-
-func _ready():
-	play("PlayLevel")
-
-func io(key, length):
-	Input.action_press(key)
-	yield(get_tree().create_timer(length), "timeout")
-	Input.action_release(key)
